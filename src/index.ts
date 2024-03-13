08a728c20e33f1aad98029efc53878173e3302dd
Damian Bast <me@damianbast.com>
2024-03-13 15:28:56 +0100

    add version check

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,30 @@ import {
   TransportKind,
 } from "coc.nvim";
 
+import { execSync } from "child_process";
+import { satisfies, coerce } from "semver";
+
 export async function activate(context: ExtensionContext): Promise<void> {
   const config = workspace.getConfiguration("coc-serverpod");
   const isEnable = config.get<boolean>("enable", true);
   if (!isEnable) {
+    return;
+  }
+
+  let output;
+  try {
+    output = execSync("serverpod version");
+  } catch (e) {
+    window.showErrorMessage(
+      "Failed to resolve the Serverpod CLI executable. Please ensure the Serverpod CLI is installed and available on the PATH used by VS Code."
+    );
+    return;
+  }
+
+  if (!validVersion(output.toString().trim())) {
+    window.showErrorMessage(
+      "The Serverpod CLI version is outdated. Please upgrade to the latest version (minimum required version is 1.2)."
+    );
     return;
   }
 
@@ -48,4 +68,17 @@ export async function activate(context: ExtensionContext): Promise<void> {
       args.length === 0 ? executable : `${executable} ${args.join(" ")}`;
     window.showMessage(`coc-serverpod: running "${cmd}"`);
   });
+}
+
+function validVersion(versionString: string): boolean {
+  console.log(versionString);
+  const versionTag = versionString.split(":")[1];
+  const versionNumber = coerce(versionTag);
+  if (versionNumber === null) {
+    // If we can't parse the version number, assume it's valid
+    // since the version format is valid for all pre 1.2 versions.
+    return true;
+  }
+
+  return satisfies(versionNumber, ">=2.2.0");
 }
