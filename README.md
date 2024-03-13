0a6e7d50b6fb983833c46a3597d5e436a7cb162f
Damian Bast <me@damianbast.com>
2024-03-13 14:37:54 +0100

    Inital commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# coc-serverpod
+
+LSP support for serverpod yaml files
+
+This is basically just a port of the VSCode extension which "only" wrapps the serverpod language server.
+
+## Install
+
+`:CocInstall coc-serverpod`
+
+## License
+
+See [LICENSE](./LICENSE)
+
+---
+
+> This extension is built with [create-coc-extension](https://github.com/fannheyward/create-coc-extension)
---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import {
+  ExtensionContext,
+  services,
+  workspace,
+  LanguageClient,
+} from "coc.nvim";
+
+export async function activate(context: ExtensionContext): Promise<void> {
+  const config = workspace.getConfiguration("coc-serverpod");
+  const isEnable = config.get<boolean>("enable", true);
+  if (!isEnable) {
+    return;
+  }
+
+  const serverOptions = {
+    command: "serverpod",
+    args: ["-q", "language-server"],
+    options: {
+      env: process.env,
+      shell: true,
+    },
+    transport: TransportKind.stdio,
+  };
+  const clientOptions = {
+    revealOutputChannelOn: RevealOutputChannelOn.Info,
+    documentSelector: [
+      { scheme: "file", language: "yaml", pattern: "**/protocol/**/*.yaml" },
+      { scheme: "file", language: "yaml", pattern: "**/models/**/*.yaml" },
+      { scheme: "file", pattern: "**/*.spy.yaml" },
+      { scheme: "file", pattern: "**/*.spy.yml" },
+      { scheme: "file", pattern: "**/*.spy" },
+    ],
+  };
+  const client = new LanguageClient(
+    "coc-serverpod", // the id
+    "coc-serverpod", // the name of the language server
+    serverOptions,
+    clientOptions
+  );
+  context.subscriptions.push(services.registLanguageClient(client));
+}
